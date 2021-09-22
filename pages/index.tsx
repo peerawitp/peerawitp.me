a35622ff282681e41586fac883f2942e55864f6b
Peerawit P <jawzainw55@hotmail.com>
2021-09-22 11:50:29 +0700

    📏 Fixed alignment on ultrawide screen
    + updated tailwindcss to 2.1.4

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
         <meta name="Home Page" content="It's me, Peerawit!" />
         <meta name="Description" content="This is a homepage of my website!" />
       </Head>
-      <section className="w-11/12 px-4 md:px-0 pt-40 md:pt-44 lg:pt-48 mx-auto md:w-3/4 lg:w-2/4 mt-0 ">
+      <section className="max-w-3xl w-11/12 px-4 md:px-0 pt-40 md:pt-44 lg:pt-48 mx-auto md:w-3/4 lg:w-2/4 mt-0">
         <Header />
         <RecentProjects />
       </section>
--- src/components/Landing/Header.tsx
+++ src/components/Landing/Header.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <main className="flex flex-col space-y-12 divide-y divide-gray-200 mb-32 ">
+    <main className="flex flex-col space-y-12 divide-y divide-gray-200 mb-32 2xl:pt-28">
       <div>
-        <p className="mt-3 sm:w-9/12 mb-2  text-blue-800 dark:text-blue-400 md:w-10/12 lg:w-full text-base md:text-lg font-bold ">
+        <p className="mt-3 sm:w-9/12 mb-2 text-blue-800 dark:text-blue-400 md:w-10/12 lg:w-full text-base md:text-lg font-bold">
           Hey there,
         </p>
         <h1 className="text-4xl md:mb-5  md:text-5xl lg:text-6xl font-extrabold leading-tight tracking-tight dark:text-gray-100  sm:leading-10   md:leading-14">
